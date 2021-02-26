90ecff38f8db57da861b98e5e2e059c5cf642b55
Arael David <cl8dep@gmail.com>
2021-02-25 19:45:49 -0500

    Cositas sexys

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
 import {join} from 'path'
 
-require('dotenv').config({path: join(__dirname, "..", ".env.production")});
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
